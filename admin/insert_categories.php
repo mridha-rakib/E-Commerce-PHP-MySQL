9f6aabbf074e77d8ac72d6fa9b2930a93ffcd40a
RAKIB <rakibmahmudmridha@gmail.com>
2023-01-29 15:27:00 +0600

    config file added and insertCategories file modified

--- admin/insert_categories.php
+++ admin/insert_categories.php
@@ -1,3 +1,6 @@
+<?php require '../config/config.php'; ?>
+
+
 <form action="" method="post" class="mb-2">
     <div class="input-group w-90 mb-2">
         <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
