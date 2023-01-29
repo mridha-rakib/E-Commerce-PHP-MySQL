e2e9814ee24b9ac4b233b216b6a2df94fd3238b3
RAKIB <rakibmahmudmridha@gmail.com>
2023-01-29 13:07:11 +0600

    files modified

--- admin/index.php
+++ admin/index.php
@@ -15,12 +15,14 @@
 
     <!-- CSS file -->
     <link rel="stylesheet" href="../style.css" />
-    <style>
-    /* .admin_image {
+    <!-- <style>
+    .admin_image {
         width: 100px;
         object-fit: contain;
     }
-    </style> */
+    </style> -->
+
+
 
 </head>
 
@@ -50,26 +52,35 @@
 
         <!-- Third child -->
         <div class="row">
-            <div class="col-md-12 bg-secondary p-1">
-                <div class="col-md-12 justify-content">
-                    <a href=""><img src="../images/icecrim.jpg" alt="" class="admin_image"></a>
-                    <p class="text-light text-center">Admin name</p>
+            <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+                <div class="p-3">
+                    <div class="col-md-12 justify-content">
+                        <a href=""><img src="../images/icecrim.jpg" alt="" class="admin_image"></a>
+                        <p class="text-light text-center">Admin name</p>
+                    </div>
+                </div>
+                <div class="button text-center">
+                    <button><a href="" class="nav-link text-light bg-info my-1">Insert Products</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View Products</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">All Orders</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">All Payments</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">List users</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
                 </div>
             </div>
-            <div class="button text-center">
-                <button><a href="" class="nav-link text-light bg-info my-1">Insert Product</a></button>
-                <button><a href="" class="nav-link text-light bg-info my-1">Edit Product</a></button>
-                <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
-                <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
-                <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
-                <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
-                <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
-                <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
-                <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
-                <button><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
-            </div>
+
+
         </div>
 
+    </div>
+
+    <!-- Footer -->
+    <div class="bg-info p-3 text-center footer">
+        <p>All right reserves @ 2023</p>
     </div>
 
 
