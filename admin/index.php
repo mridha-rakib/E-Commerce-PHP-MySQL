b830115f5ada35e014d220ad45e39d87cab31884
RAKIB <rakibmahmudmridha@gmail.com>
2023-01-29 10:34:13 +0600

    admin folder and file added

---
+++ admin/index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>E-Commerce</title>
+    <!-- bootstrap css link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- CSS file -->
+    <link rel="stylesheet" href="../style.css" />
+
+</head>
+
+<body>
+
+    <!-- navbar -->
+    <div class="container-fluid">
+        <!-- First child -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <div class="container-fluid">
+                <img src="../images/admin.jpg" alt="" class="logo">
+                <nav class="navbar navbar-expand-lg">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Welcome Guest</a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </nav>
+
+        <!-- Second Child -->
+        <div class="bg-light">
+            <h3 class="text-center p-2">Manage Details</h3>
+        </div>
+
+        <!-- Third child -->
+        <div class="row">
+            <div class="col-md-12 bg-secondary p-1">
+                <div><a href=""></a><img src="../images/" alt=""></div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <!-- bootstrap js link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
