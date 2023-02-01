6e16f26977597439e95ff246ec7d3a580a55c7e2
me-badsha <badshafaysa@gmail.com>
2023-02-01 12:57:38 +0600

    some code added

--- auth/registration.php
+++ auth/registration.php
@@ -23,9 +23,9 @@
     </main>
     <form>
         <div class="cul-mb-6">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <label for="exampleInputEmail1" class="form-label">Your Name</label>
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
