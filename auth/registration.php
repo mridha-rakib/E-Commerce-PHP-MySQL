85e1843d8d7687095f7e256ccfbe32b4fc6e0320
RAKIB <rakibmahmudmridha@gmail.com>
2023-02-01 12:50:16 +0600

    files modified

--- auth/registration.php
+++ auth/registration.php
@@ -14,40 +14,64 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
         integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- tel input JS  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"></script>
+
 
 </head>
 
 <body class="bg-custom overflow-auto">
     <main>
-        <div clas="d-flex flex-column"></div>
+        <div class="d-flex flex-column justify-content-center align-items-center">
+            <div class="p-1 w-md-50 h-100 d-flex justify-content-center">
+                <div class="card p-2 p-md-5 m-md-5 shadow w-sm-50 ">
+                    <h1 class="card-title text-center text-custom-red">Registration Form</h1>
+                    <form>
+                        <div class="cul-mb-6 mb-2">
+                            <label for="username" class="form-label">Full Name</label>
+                            <input type="text" class="form-control" id="username" placeholder="Enter your full name">
+                        </div>
+                        <div class="cul-mb-6 mb-2">
+                            <label for="phone" class="form-label">Contact Number</label>
+                            <input type="tel" name="phone" class="form-control" id="phone"
+                                placeholder="Enter your phone number">
+                            <!-- <input type="submit" class="btn" value="Verify" /> -->
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="useremail" class="form-label">Email address</label>
+                            <input type="email" name="email" class="form-control" id="useremail"
+                                placeholder="Enter your email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </main>
-    <form>
-        <div class="cul-mb-6">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
 
 
+
+
+    <!-- <script>
+    const phoneInputField = document.querySelector("#phone");
+    const phoneInput = window.intlTelInput(phoneInputField, {
+        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    });
+    </script> -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-        crossorigin="anonymous"></script>
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 
 </body>
 
